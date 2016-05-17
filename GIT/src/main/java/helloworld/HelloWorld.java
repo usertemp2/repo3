f520c02409af05a2f6478e16c65f7a454d6b547e
usertemp2 <c2359993@trbvn.com>
2016-05-17 19:56:08 +0530

    commit 1
    Commit 1

---
+++ GIT/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+interface InterfaceTest
+{
+	//default void a(){}
+}
+
+class ClassTest{}
+
+
+public class HelloWorld {
+
+	public static void main(String args[]){
+
+		System.out.println("Hello World, Maven");
+
+	}
+
+}
